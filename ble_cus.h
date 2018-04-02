36cc7854a7c7c1986b378e9e6f2a3efae0cfef4a
amocsrotkiv <38005489+amocsrotkiv@users.noreply.github.com>
2018-04-02 15:07:14 +0200

    Add files via upload

---
+++ ble_cus.h
@@ -0,0 +1,128 @@
+#ifndef BLE_CUS_H__
+#define BLE_CUS_H__
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "ble.h"
+#include "ble_srv_common.h"
+
+/**@brief   Macro for defining a ble_hrs instance.
+ *
+ * @param   _name   Name of the instance.
+ * @hideinitializer
+ */
+ /*Make sure that our ble_cus_on_ble_evt event handler function receives SoftDevice events.
+ This is done registering the ble_cus_on_ble_evt event handler as event observer using the NRF_SDH_BLE_OBSERVER() macro.*/
+#define BLE_CUS_DEF(_name)   /*Ez a Makro létrehoz egy custom_service típusú változót */                                                                       \
+static ble_cus_t _name;    /*Ennek a két sornak a pédányosítása a main.c-ben van*/                                                                         \
+NRF_SDH_BLE_OBSERVER(_name ## _obs,                                                                 \
+                     BLE_HRS_BLE_OBSERVER_PRIO,                                                     \
+                     ble_cus_on_ble_evt, &_name)
+
+
+
+// CUSTOM_SERVICE_UUID_BASE f364adc9-b000-4042-ba50-05ca45bf8abc
+
+#define CUSTOM_SERVICE_UUID_BASE         {0xBC, 0x8A, 0xBF, 0x45, 0xCA, 0x05, 0x50, 0xBA, \
+                                          0x40, 0x42, 0xB0, 0x00, 0xC9, 0xAD, 0x64, 0xF3}
+
+#define CUSTOM_SERVICE_UUID               0x1400
+#define CUSTOM_VALUE_CHAR_UUID            0x1401
+#define VIKTOR_VALUE_CHAR_UUID			  0x5555
+																					
+/**@brief Custom Service event type. */
+typedef enum
+{
+    BLE_CUS_EVT_NOTIFICATION_ENABLED,                             /**< Custom value notification enabled event. */
+    BLE_CUS_EVT_NOTIFICATION_DISABLED,                             /**< Custom value notification disabled event. */
+    BLE_CUS_EVT_DISCONNECTED,
+    BLE_CUS_EVT_CONNECTED
+} ble_cus_evt_type_t;
+
+/**@brief Custom Service event. */
+typedef struct
+{
+    ble_cus_evt_type_t evt_type;                                  /**< Type of event. */
+} ble_cus_evt_t;
+
+// Forward declaration of the ble_cus_t type.
+typedef struct ble_cus_s ble_cus_t; // Egy típust hoztunk létre a service struktúránkhoz
+
+
+/**@brief Custom Service event handler type. */
+typedef void (*ble_cus_evt_handler_t) (ble_cus_t * p_bas, ble_cus_evt_t * p_evt);
+
+/**	A service inicializálásához
+	@brief Battery Service init structure. This contains all options and data needed for
+ *        initialization of the service.*/
+typedef struct
+{
+    ble_cus_evt_handler_t         evt_handler;                    /**< Event handler to be called for handling events in the Custom Service. */
+    char                         initial_custom_value[20];           /** Itt ez valami kezdeti érték< Initial custom value */
+	ble_srv_cccd_security_mode_t  custom_value_char_attr_md;     /**< Initial security level for Custom characteristics attribute */
+	uint8_t						  initial_viktor_value;
+	ble_srv_cccd_security_mode_t  viktor_value_char_attr_md;     /**< Initial security level for Custom characteristics attribute */
+} ble_cus_init_t;
+
+/**  Az adott szervizünk struktúrája.Minden a servicehez tartozó információt
+     és adatot itt tárolunk. Pl a handlet az értékéhez vagy a kapcsolat handle-jét
+@brief Custom Service structure. This contains various status information for the service. */
+struct ble_cus_s
+{
+    ble_cus_evt_handler_t         evt_handler;                    /**< Event handler to be called for handling events in the Custom Service. */
+    uint16_t                      service_handle;                 /**<  Handle of Custom Service (as provided by the BLE stack). */
+    ble_gatts_char_handles_t      custom_value_handles;           /**< Ez tartja a karakterisztika handle-jét..Handles related to the Custom Value characteristic. */
+    uint16_t                      conn_handle;                    /**< Handle of the current connection (as provided by the BLE stack, is BLE_CONN_HANDLE_INVALID if not in a connection). */
+    uint8_t                       uuid_type; 	// ez a mező kell mikor a BLE UUID táblájához hozzáadjuk a saját Base UUIdnkat
+	ble_gatts_char_handles_t      viktor_value_handles;
+};
+
+/**@brief Function for initializing the Custom Service.
+ *
+ * @param[out]  p_cus       Custom Service structure. This structure will have to be supplied by
+ *                          the application. It will be initialized by this function, and will later
+ *                          be used to identify this particular service instance.
+ * @param[in]   p_cus_init  Information needed to initialize the service.
+ *
+ * @return      NRF_SUCCESS on successful initialization of service, otherwise an error code.
+ */
+uint32_t ble_cus_init(ble_cus_t * p_cus, const ble_cus_init_t * p_cus_init);
+
+/**@brief Function for handling the Application's BLE Stack events.
+ *
+ * @details Handles all events from the BLE stack of interest to the Battery Service.
+ *
+ * @note 
+ *
+ * @param[in]   p_cus      Custom Service structure.
+ * @param[in]   p_ble_evt  Event received from the BLE stack.
+ */
+void ble_cus_on_ble_evt( ble_evt_t const * p_ble_evt, void * p_context);
+
+/**@brief Function for updating the custom value.
+ *
+ * @details The application calls this function when the custom value should be updated. If
+ *          notification has been enabled, the custom value characteristic is sent to the client.
+ *
+ * @note 
+ *       
+ * @param[in]   p_bas          Custom Service structure.
+ * @param[in]   Custom value 
+ *
+ * @return      NRF_SUCCESS on success, otherwise an error code.
+ */
+
+uint32_t ble_cus_custom_value_update(ble_cus_t * p_cus, uint8_t custom_value);
+
+/*Ez eredetileg nem is volt itt !? This code belongs in ble_cus.h*/
+
+/**@brief Function for adding the Custom Value characteristic.
+ *
+ * @param[in]   p_cus        Custom Service structure.
+ * @param[in]   p_cus_init   Information needed to initialize the service.
+ *
+ * @return      NRF_SUCCESS on success, otherwise an error code.
+ */
+static uint32_t custom_value_char_add(ble_cus_t * p_cus, const ble_cus_init_t * p_cus_init);
+
+#endif // BLE_CUS_H__
